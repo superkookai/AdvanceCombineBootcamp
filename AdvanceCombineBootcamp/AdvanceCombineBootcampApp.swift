ac5492d3825dbaf30633f221ff5b541013c3c473
superkookai <weerawut@live.com>
2025-03-14 17:57:19 +0700

    Initial Commit

---
+++ AdvanceCombineBootcamp/AdvanceCombineBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceCombineBootcampApp.swift
+//  AdvanceCombineBootcamp
+//
+//  Created by Weerawut Chaiyasomboon on 14/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceCombineBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
